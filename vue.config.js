731ed17e1a9e474bdd327069b28ab611427b2cc0
Jawad Ahmed <jawadahmedtaj@gmail.com>
2021-03-11 07:34:55 +0100

    fixed config

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  pwa: {
-    name: 'Accel Thesis',
-    themeColor: '#4DBA87',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'no',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
-  }
+  transpileDependencies: ["vuetify"]
 };
